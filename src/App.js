d7065badb168239bacdea446d7ee9c5090d524e7
Christian Pereira <christianpbsd@gmail.com>
2020-10-29 23:33:38 -0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export default () => {
+  return(
+    <div className="app-window">
+        <div className="sidebar">
+           <header>
+              <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt=""/>
+              <div className="header--buttons">
+                  <div className="header--btn">
+                      <DonutLargeIcon style={{color:'#919191'}}/>
+                  </div>
+                  <div className="header--btn">
+                      <ChatIcon style={{color:'#919191'}}/>
+                  </div>
+                  <div className="header--btn">
+                      <MoreVertIcon style={{color:'#919191'}}/>
+                  </div>
+              </div>
+           </header>
+
+           <div className="search">
+              ...
+           </div>
+
+           <div className="chatList">
+              ...
+           </div>
+        </div>
+        <div className="contentarea">
+           ...
+        </div>
     </div>
   );
 }
-
-export default App;

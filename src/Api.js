f4ed6afffac5eccc1849bb6fa5fb7087fe2cebe8
Christian Pereira <christianpbsd@gmail.com>
2021-02-05 14:00:21 -0300

    confing firebase banco de dados

---
+++ src/Api.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/firebase-firestore';
+
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebase);
+const db = firebaseApp.firestore();
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBumwDS_aCSmCcK5x_Wb-fqIMMviUcTFQI",
+    authDomain: "whatsappclone-80f2e.firebaseapp.com",
+    projectId: "whatsappclone-80f2e",
+    storageBucket: "whatsappclone-80f2e.appspot.com",
+    messagingSenderId: "746360173151",
+    appId: "1:746360173151:web:32791c8e0e8718ef4761ea"
+  };

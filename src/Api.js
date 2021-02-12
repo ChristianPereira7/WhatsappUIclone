cdb8ae158e18e7fbba1fb9e3d233d4f67f0138c4
Christian Pereira <christianpbsd@gmail.com>
2021-02-11 21:44:23 -0300

    Update

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firebase-auth';
 import 'firebase/firebase-firestore';
 
-import firebaseConfig from './firebaseConfig';
+import firebaseConfig from './firebaseConfi';
 
 const firebaseApp = firebase.initializeApp(firebase);
 const db = firebaseApp.firestore();
+
+
+export default {
+    fbPopup: async () => {
+        const provider = new firebase.auth.FacebookAuthProvider();
+        
+    }
+};
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '.Login.css';
+
+
+export default () => {
+
+    const handleFacebookLogin = () = {
+        
+    }
+
+    return (
+        <div className="login">
+            <button onClick={handleFacebookLogin}>Logar com Facebook</button>
+        </div>
+    );
+}
